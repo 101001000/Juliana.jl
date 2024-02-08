28d5fa553dd5f8d6b643f2b671d828d997d319f5
101001000 <kike.delacalle.montilla@gmail.com>
2024-02-08 14:42:37 +0100

    main splitted

--- src/CUDATranslator.jl
+++ src/CUDATranslator.jl
@@ -417,17 +417,7 @@ function replace_cuda(str)
 end
 
 
- 
-file_input = open(ARGS[1], "r")
-file_output = open(ARGS[2], "w")
-target_backend = ARGS[3]
 
-
-str = read(file_input, String)
-write(file_output, replace_cuda("begin " * str * " end"))
-
-close(file_input)
-close(file_output)
 
 
 
