3ba7a1ea3a3a1b5cc981f02eb542380fd2f2e37d
101001000 <kike.delacalle.montilla@gmail.com>
2024-02-08 13:37:20 +0100

    First translation CUDA approach. Working on rodinia nn

---
+++ src/CUDA.jl
@@ -0,0 +1,419 @@
+import MacroTools
+import CUDA
+
+# using MyModule
+# in-scope: All exported names (x and y), MyModule.x, MyModule.y, and MyModule.p
+# extensible: MyModule.x, MyModule.y, and MyModule.p
+# using MyModule: x, p
+# in-scope: x and p
+# extensible: (nothing)
+# import MyModule
+# in-scope: MyModule.x, MyModule.y, and MyModule.p
+# extensible: MyModule.x, MyModule.y, and MyModule.p
+# import MyModule.x, MyModule.p
+# in-scope: x and p
+# extensible: x and p
+# import MyModule: x, p
+# in-scope: x and p
+# extensible: x and p
+
+
+# Return a sorted list of the global imports. TODO
+#function extract_modules(top_expr)
+#    return["CUDA"]
+#end
+#
+#function get_namespace(top_expr, global_symbol)
+#    using_modules = extract_modules(top_expr)
+#    namespace = nothing
+#    for modul in using_modules
+#        names = names(modul)
+#        for name in names
+#            if global_symbol == Symbol(name)
+#                namespace = name
+#            end
+#        end
+#    end
+#end
+
+
+# Expression matcher:
+# contains_symbol
+# exact_string
+# 
+#
+
+
+kernel_ids = []
+
+
+function extract_kernel_name_from_call(expr)
+    return expr.args[5].args[1] #TODO, hacer esto bien
+end
+
+function add_kernel_macro!(expr, sym)
+    for i in eachindex(expr.args)
+        if typeof(expr.args[i]) != Expr 
+            continue
+        end
+        if expr.args[i].head == :function
+            if(expr.args[i].args[1].args[1] == sym)
+                expr.args[i] = Expr(:macrocall, Symbol("@kernel"), LineNumberNode(1), expr.args[i])
+                continue
+            end
+        end
+        add_kernel_macro!(expr.args[i], sym)
+    end
+end
+
+
+function expr_to_string(expr)
+    io = IOBuffer()
+    Base.show_unquoted(io, expr, 0, -1)
+    return String(take!(io))
+end
+
+function expr_list_to_string(list)
+    str = ""
+    for el in list
+        str *= expr_to_string(el) * "\n"
+    end
+    return str
+end
+
+
+function expr_identify(expr, str)
+    if typeof(expr) != Expr
+        return false
+    end
+    return expr_to_string(expr) == str
+end
+
+function expr_identify_1(expr, str)
+    try    
+        return expr_to_string(expr.args[1]) == str
+    catch
+        return false
+    end
+end
+
+function expr_identify_any(expr, str)
+    res = false
+    try
+        for arg in expr.args
+            if expr_to_string(arg) == str
+                res = true
+            end
+            res |= expr_identify_any(arg, str)
+        end
+    catch
+    end
+    return res
+end
+
+function expr_identify_line(expr, str)
+    if typeof(expr) == Expr
+        return expr.head != Symbol("block") && expr_identify_any(expr, str) && expr.head != Symbol("function") && expr.head != Symbol("if") 
+    end
+    return false
+end
+
+function expr_identify_n(expr, str)
+    try
+        for arg in expr.args
+            if expr_identify_any(arg, str)
+                return true
+            end
+        end
+    catch
+    end
+    return false
+end
+
+
+
+function expr_has_symbol(expr, sym)
+    if typeof(expr) == Symbol
+        return sym == expr
+    else
+        has_symbol = false
+        for arg in expr.args
+            has_symbol |= expr_has_symbol(arg, sym)
+        end
+        return has_symbol
+    end
+end
+
+
+function warning_generator(expr)
+    if typeof(expr) == Expr
+        for arg in expr.args
+            if arg == Symbol("CUDA") || arg == Symbol("NVTX") 
+                display("UNTRANSLATED SYMBOL")
+                display(expr)
+            end
+            warning_generator(arg)
+        end
+    end
+end
+
+function namespace_replacer(expr)
+    for i in eachindex(expr.args)
+
+        arg = expr.args[i]
+
+        if expr.head == Symbol("import")
+            continue
+        end
+
+        new_expr = expr_replacer(arg)
+
+        if !isnothing(new_expr)
+            expr.args[i] = new_expr
+        end
+
+        if typeof(arg) == Expr
+            namespace_replacer(arg)
+        end 
+
+    end
+end
+
+function symbol_replace!(expr, old_symbol, new_symbol)
+    if typeof(expr) == Expr
+        for i in eachindex(expr.args)
+            if typeof(expr.args[i]) == QuoteNode
+                if expr.args[i].value == Symbol(old_symbol)
+                    expr.args[i] = QuoteNode(Symbol(new_symbol))
+                end
+            end
+            if expr.args[i] == Symbol(old_symbol)
+                expr.args[i] = Symbol(new_symbol)
+            end
+            symbol_replace!(expr.args[i], old_symbol, new_symbol)
+        end
+    end
+end
+
+function generate_kernel_call(expr)
+
+    fun_call = nothing
+    threads = nothing
+    blocks = nothing
+
+    for arg in expr.args
+        if typeof(arg) != Expr
+            continue
+        end
+        if arg.head == Symbol("call")
+            fun_call = arg
+        end
+        if arg.head == Symbol("=")
+            for i in eachindex(arg.args)
+                subarg = arg.args[i]
+                if subarg == Symbol("threads")
+                    threads = arg.args[i+1]
+                end
+                if subarg == Symbol("blocks")
+                    blocks = arg.args[i+1]
+                end
+            end
+        end
+    end
+
+    blocks_x = blocks.args[1]
+    blocks_y = blocks.args[2]
+
+    first_call = Expr(:call, fun_call.args[1], :backend, threads)
+
+    ndrange_par = Expr(:kw, :ndrange, Expr(:tuple, Expr(:call, :*, blocks_x, threads), Expr(:call, :*, blocks_y, threads)))
+
+    second_call = Expr(:call, first_call, fun_call.args[2:end]..., ndrange_par)
+
+    return second_call
+end
+
+
+#ASSUME CUDA NAMESPACE
+function expr_replacer(expr)
+
+    if typeof(expr) != Expr
+        return nothing
+    end
+
+    if     expr_identify(expr, "(CUDA.threadIdx()).x")
+        return Meta.parse("KernelAbstractions.@index(Local, Cartesian)[1]")
+    elseif expr_identify(expr, "(CUDA.threadIdx()).y")
+        return Meta.parse("KernelAbstractions.@index(Local, Cartesian)[2]")
+    elseif expr_identify(expr, "(CUDA.blockIdx()).x")
+        return Meta.parse("KernelAbstractions.@index(Group, Cartesian)[1]")
+    elseif expr_identify(expr, "(CUDA.blockIdx()).y")
+        return Meta.parse("KernelAbstractions.@index(Group, Cartesian)[2]")
+    elseif expr_identify(expr, "(CUDA.blockDim()).x")
+        return Meta.parse("KernelAbstractions.@groupsize()[1]")
+    elseif expr_identify(expr, "(CUDA.blockDim()).y")
+        return Meta.parse("KernelAbstractions.@groupsize()[2]")
+    elseif expr_identify(expr, "(CUDA.gridDim()).x")
+        return Meta.parse("ceil(Int, KernelAbstractions.@ndrange()[1] / KernelAbstractions.@groupsize()[1])")
+    elseif expr_identify(expr, "(CUDA.gridDim()).y")
+        return Meta.parse("ceil(Int, KernelAbstractions.@ndrange()[2] / KernelAbstractions.@groupsize()[2])")
+    elseif expr_identify_1(expr, "CUDA.synchronize")
+        new_expr = copy(expr)
+        symbol_replace!(new_expr, "CUDA", "KernelAbstractions")
+        push!(new_expr.args, Symbol("backend") )
+        return new_expr
+    elseif expr_identify(expr, "CUDA.sqrt")
+        new_expr = copy(expr)
+        symbol_replace!(new_expr, "CUDA", "KernelAbstractions")
+        return new_expr
+    elseif expr_identify_1(expr, "CUDA.CuArray{Float32}")
+        new_expr = copy(expr)
+        symbol_replace!(new_expr, "CUDA", "KAUtils")
+        symbol_replace!(new_expr, "CuArray", "ArrayConstructor")
+        insert!(new_expr.args, 2, Symbol("backend"))
+        insert!(new_expr.args, 3, extract_type_from_curly_call(expr))
+        new_expr.args[1] = uncurlyfy(new_expr.args[1])
+        return new_expr
+    elseif expr_identify_1(expr, "CUDA.CuArray")
+        new_expr = copy(expr)
+        symbol_replace!(new_expr, "CUDA", "KAUtils")
+        symbol_replace!(new_expr, "CuArray", "ArrayConstructor")
+        insert!(new_expr.args, 2, Symbol("backend"))
+        return new_expr
+    elseif expr_identify_1(expr, """CUDA.var\"@cuda\"""")
+        push!(kernel_ids, extract_kernel_name_from_call(expr))
+        return generate_kernel_call(expr)
+    elseif expr_identify(expr, """\$(Expr(:., :NVTX))""")
+        return Expr(:., :KernelAbstractions)
+    elseif expr_identify(expr, """\$(Expr(:., :CUDA))""")
+        return Expr(:., :KernelAbstractions)
+
+    ## FORCED VALUES TODO, MAKE THIS PARAMETRIC
+    elseif expr_identify(expr, "CUDA.attribute(dev, CUDA.DEVICE_ATTRIBUTE_MAX_THREADS_PER_BLOCK)")
+        return Meta.parse("256")
+    elseif expr_identify(expr, "CUDA.attribute(dev, CUDA.DEVICE_ATTRIBUTE_MAX_GRID_DIM_X)") 
+        return Meta.parse("1024")
+    elseif expr_identify_1(expr, "CUDA.available_memory") 
+        return Meta.parse("1024*1024*1024")
+
+    ## COMMENT VALUES
+    elseif expr_identify_line(expr, "CUDA.var\"@profile\"")  ## TODO, EMIT WARNING
+        return Meta.parse("""KAUtils.@comment "Line removed by incompatibility"  """ )
+    elseif expr_identify_line(expr, "CUDA.device") 
+        return Meta.parse("""KAUtils.@comment "Line removed by incompatibility"  """ )
+    else
+        return nothing
+    end
+end
+
+
+
+function add_namespace(expr, namespace)
+    if typeof(expr) == Symbol
+        return Expr(Symbol("."), Symbol(namespace), QuoteNode(expr))
+    else
+        new_expr = copy(expr)
+
+        if typeof(new_expr.args[1]) == Expr
+            new_expr.args[1].args[1] = Expr(Symbol("."), Symbol(namespace), QuoteNode(new_expr.args[1].args[1]))
+        else
+            new_expr.args[1] = Expr(Symbol("."), Symbol(namespace), QuoteNode(new_expr.args[1]))
+        end
+        return new_expr
+    end
+end
+
+function uncurlyfy(expr)
+    if typeof(expr) != Expr
+        return expr
+    end
+    if expr.head == Symbol("curly")
+        return expr.args[1]
+    end
+    return expr
+end
+
+#Extract type from a curly expression
+function extract_type_from_curly_call(expr)
+    @assert expr.head == :call
+    @assert expr.args[1].head == :curly
+    return expr.args[1].args[2]
+end
+
+
+
+function function_call_is_in_cuda_namespace(expr)
+    @assert typeof(expr) == Expr
+    @assert expr.head == Symbol("call") || expr.head == Symbol("macrocall")
+    n = names(CUDA)
+    return uncurlyfy(expr.args[1]) in n
+end
+
+function explicit_using_replace!(expr)
+
+    if typeof(expr) != Expr
+        return
+    end
+
+    for i in eachindex(expr.args)
+
+        arg = expr.args[i]
+
+        if typeof(arg) != Expr
+            continue
+        end
+
+        if arg.head == Symbol("call") || arg.head == Symbol("macrocall")
+            if function_call_is_in_cuda_namespace(arg)
+                expr.args[i] = add_namespace(arg, "CUDA") 
+            end
+        end
+        explicit_using_replace!(expr.args[i])
+    end
+end
+
+
+
+
+function replace_cuda(str)
+
+    ast_top = Meta.parse(str)
+    explicit_using_replace!(ast_top)
+    namespace_replacer(ast_top)
+    warning_generator(ast_top)
+
+    #string_representation = "@kernel " * string_representation TODO 
+
+    pushfirst!(ast_top.args, Meta.parse("backend = oneAPIBackend()"))
+    pushfirst!(ast_top.args, Expr(:using, Expr(:., :., :KAUtils)))
+    pushfirst!(ast_top.args, Expr(:call, :include, String(@__DIR__) * "/KAUtils.jl"))
+    pushfirst!(ast_top.args, Meta.parse("using oneAPI"))
+
+    for id in kernel_ids
+        add_kernel_macro!(ast_top, id)
+    end
+
+    
+
+
+    #include("/homelocal/encalle_local/Desktop/Julia/kernelAbstractor/src/KAUtils.jl")
+    #using .KAUtils
+
+    ast_top = MacroTools.striplines(ast_top)
+    return expr_list_to_string(ast_top.args) #necessary to remove the initial block.
+end
+
+
+ 
+file_input = open(ARGS[1], "r")
+file_output = open(ARGS[2], "w")
+
+str = read(file_input, String)
+write(file_output, replace_cuda("begin " * str * " end"))
+
+close(file_input)
+close(file_output)
+
+
+
+
