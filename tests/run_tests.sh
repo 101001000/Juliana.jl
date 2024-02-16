75778c4041db946141ca4180b62761651c3f55e9
101001000 <kike.delacalle.montilla@gmail.com>
2024-02-16 13:05:57 +0100

    test script split

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,16 +1,20 @@
-backend=ONEAPI
-
-rm rodinia/julia_gen/ -d -r -f
-cp rodinia/julia_cuda/ rodinia/julia_gen/ -d -r
-find rodinia/julia_gen/ -name "*.jl" -type f -delete
-
-julia ../src/main.jl rodinia/julia_cuda/backprop/backprop.jl rodinia/julia_cuda/backprop/backprop_cuda.jl rodinia/julia_cuda/backprop/backprop_cuda_kernel.jl rodinia/julia_cuda/backprop/facetrain.jl rodinia/julia_gen/backprop/ $backend
-julia ../src/main.jl rodinia/julia_cuda/bfs/bfs.jl rodinia/julia_gen/bfs/ $backend
-julia ../src/main.jl rodinia/julia_cuda/hotspot/hotspot.jl rodinia/julia_gen/hotspot/ $backend
-julia ../src/main.jl rodinia/julia_cuda/leukocyte/find_ellipse.jl rodinia/julia_cuda/leukocyte/find_ellipse_kernel.jl rodinia/julia_cuda/leukocyte/leukocyte.jl rodinia/julia_cuda/leukocyte/misc_math.jl rodinia/julia_cuda/leukocyte/track_ellipse.jl rodinia/julia_cuda/leukocyte/track_ellipse_kernel.jl rodinia/julia_gen/leukocyte/ $backend
-julia ../src/main.jl rodinia/julia_cuda/lud/common.jl rodinia/julia_cuda/lud/lud.jl rodinia/julia_cuda/lud/lud_kernel.jl rodinia/julia_gen/lud/ $backend
-julia ../src/main.jl rodinia/julia_cuda/nn/nn.jl rodinia/julia_gen/nn/ $backend
-julia ../src/main.jl rodinia/julia_cuda/nw/needle.jl rodinia/julia_cuda/nw/needle_kernel.jl rodinia/julia_gen/nw/ $backend
-julia ../src/main.jl rodinia/julia_cuda/particlefilter/particlefilter_double.jl rodinia/julia_gen/particlefilter/ $backend
-julia ../src/main.jl rodinia/julia_cuda/pathfinder/pathfinder.jl rodinia/julia_gen/pathfinder/ $backend
-julia ../src/main.jl rodinia/julia_cuda/streamcluster/streamcluster.jl rodinia/julia_cuda/streamcluster/streamcluster_cuda.jl rodinia/julia_cuda/streamcluster/streamcluster_header.jl rodinia/julia_gen/streamcluster/ $backend
+echo Testing Backprop
+(cd rodinia/julia_gen/backprop/; ./verify)
+echo Testing BFS
+(cd rodinia/julia_gen/bfs/; ./verify)
+echo Testing Hotspot
+(cd rodinia/julia_gen/hotspot/; ./verify)
+echo Testing Leukocyte
+(cd rodinia/julia_gen/leukocyte/; ./verify)
+echo Testing LUD
+(cd rodinia/julia_gen/lud/; ./verify)
+echo Testing NN
+(cd rodinia/julia_gen/nn/; ./verify)
+echo Testing NW
+(cd rodinia/julia_gen/nw/; ./verify)
+echo Testing ParticleFilter
+(cd rodinia/julia_gen/particlefilter/; ./verify)
+echo Testing PathFinder
+(cd rodinia/julia_gen/pathfinder/; ./verify)
+echo Testing StreamCluster
+(cd rodinia/julia_gen/streamcluster/; ./verify)
