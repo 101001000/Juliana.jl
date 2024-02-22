23e3fc75ea89a482deecc6838f1dbcf627805f4d
101001000 <kike.delacalle.montilla@gmail.com>
2024-02-22 12:32:10 +0100

    Program arguments

--- src/main.jl
+++ src/main.jl
@@ -1,44 +1,145 @@
+using ArgParse
+using FilePathsBase
+
 include("CUDATranslator.jl")
 
-kernel_ids = Set()
-asts = []
-
-for file_name in ARGS[1:end-2]
-
-    println("Loading ", file_name)
-
-    file_input = open(file_name, "r")
-    str = read(file_input, String)
-    str = "begin " * str * " end"
-    close(file_input)
-
-    #str = replace_comments(str)
-    ast = Meta.parse(str)
-
-    explicit_using_replace!(ast)
-    extract_kernel_names!(ast, kernel_ids)
-    push!(asts, ast)
-end
-
-for id in kernel_ids
-    for ast in asts
-        kernelize_function!(ast, id)
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--input", "-i"
+            help = "input files separated by a comma, or directory"
+            required = true
+        "--output", "-o"
+            help = "output files separated by a comma, or directory"
+            required = true
+        "--recursive", "-r"
+            help = "travel directories recursively"
+            action = :store_true
+        "--prefix", "-p"
+            help = "prefix for translated files"
+        "--backend", "-b"
+            help = "output backend desired"
+            default = "CUDA"
+        "--comments", "-c"
+            help = "enable comments"
+        "--mirror", "-m"
+            help = "mirror the input file structure in the output"
+            action = :store_true
     end
+    return parse_args(s)
+end
+
+function get_files_from_dir(path, recursive)
+    files = []
+    str_list = readdir(path)
+    str_list = map(x -> path * "/" * x, str_list)
+    for file in str_list
+        if isfile(file) && splitext(file)[2] == ".jl"
+            push!(files, file)
+        elseif isdir(file)
+            if recursive
+                append!(files, get_files_from_dir(file, recursive))
+            end
+        else
+            #println("ehhhmmmm $file")
+        end
+    end
+    return files
+end
+
+function retrieve_input_files(files_str_list, recursive)
+    files = []
+    for path in files_str_list
+        if isfile(path)
+            push!(files, path)
+        elseif isdir(path)
+            append!(files, get_files_from_dir(path, recursive))
+        else
+            println("$path does not exist")
+        end
+    end
+    return files
+end
+
+function retrieve_files(parsed_args)
+
+    input_paths  = split(parsed_args["input"], ",")
+    output_paths = split(parsed_args["output"], ",")
+
+    if isdir(input_paths[1]) && !isfile(output_paths[1]) && length(input_paths) == 1 && length(output_paths) == 1
+        println("dir mode")
+        input_files  = retrieve_input_files(input_paths, parsed_args["recursive"])
+        output_files = map(x -> replace(x, input_paths[1] => output_paths[1]), input_files)
+    elseif length(input_paths) == length(output_paths) && all(path -> isfile(path), input_paths) && all(path -> !isdir(path), output_paths)
+        println("file mode")  
+        input_files = input_paths
+        output_files = output_paths
+    else
+        println("wrong usage")
+        for path in input_paths
+            if !isfile(path)
+                println(path, " not found")
+            end
+        end
+    end
+
+    if !isnothing(parsed_args["prefix"])
+        output_files = map(x -> dirname(x) * "/" * parsed_args["prefix"] * basename(x), output_files)
+    end
+
+    return input_files, output_files
 end
 
 
-for i in eachindex(ARGS[1:end-2])
+function main()
 
-    file_input = ARGS[i]
+    parsed_args = parse_commandline()
+    input_files, output_files = retrieve_files(parsed_args)
 
-    println("Outputing ", ARGS[end-1] * basename(file_input))
+    kernel_ids = Set()
+    asts = []
 
+    for file_name in input_files
 
-    ast = replace_cuda_1(asts[i])
-    str = replace_cuda_2(ast, ARGS[end])
-    #str = string(i)
-    file_output = open(ARGS[end-1] * basename(file_input), "w")
-    write(file_output, str)
-    close(file_output)
+        println("Loading ", file_name)
+    
+        file_input = open(file_name, "r")
+        str = read(file_input, String)
+        str = "begin " * str * " end"
+        close(file_input)
+    
+        if !isnothing(parsed_args["comments"])
+            str = replace_comments(str)
+        end
+        
+        ast = Meta.parse(str)
+    
+        explicit_using_replace!(ast)
+        extract_kernel_names!(ast, kernel_ids)
+        push!(asts, ast)
+    end
+
+    for id in kernel_ids
+        for ast in asts
+            kernelize_function!(ast, id)
+        end
+    end
+    
+
+    for i in eachindex(output_files)
+        println("Outputing ", output_files[i])
+        ast = replace_cuda_1(asts[i])
+        str = replace_cuda_2(ast, parsed_args["backend"])
+        #str = string(i)
+        mkpath(dirname(output_files[i]))
+        file_output = open(output_files[i], "w")
+        write(file_output, str)
+        close(file_output)
+    end
+
 
 end
+
+main()
+
+
