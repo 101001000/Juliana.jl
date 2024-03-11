863eee3da56025454128f69ff54dd0c6f22ec979
101001000 <kike.delacalle.montilla@gmail.com>
2024-03-11 15:09:17 +0100

    clean results script

---
+++ tests/clean_results.sh
@@ -0,0 +1,3 @@
+find rodinia/julia_cuda/ -type f -name "*.json" -exec rm -f {} +
+find rodinia/julia_gen/ -type f -name "*.json" -exec rm -f {} +
+find rodinia/common/julia/csv -type f -name "*.csv" -exec rm -f {} +
