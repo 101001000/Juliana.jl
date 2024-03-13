c5c06b19dbf8bf2d4171bef70feb479cf236d647
Enrique de la Calle <encalle@ucm.es>
2024-03-13 15:39:22 +0100

    clean ExaTronKernels results

--- tests/clean_results.sh
+++ tests/clean_results.sh
@@ -1,3 +1,6 @@
 find rodinia/julia_cuda/ -type f -name "*.json" -exec rm -f {} +
 find rodinia/julia_gen/ -type f -name "*.json" -exec rm -f {} +
 find rodinia/common/julia/csv -type f -name "*.csv" -exec rm -f {} +
+find ExaTronKernels.jl/results/cuda/ -type f -name "*.json" -exec rm -f {} +
+find ExaTronKernels.jl/results/gen/ -type f -name "*.json" -exec rm -f {} +
+find ExaTronKernels.jl/results/csv/ -type f -name "*.csv" -exec rm -f {} +
