553b4e3904f3ae4f88e0ddddf10cb6f225a9e150
101001000 <kike.delacalle.montilla@gmail.com>
2024-04-10 15:43:20 +0200

    updated transform script

--- tests/translate_tests.sh
+++ tests/translate_tests.sh
@@ -1,11 +1,12 @@
-backend=CPU
+backend=CUDA
 
 rm rodinia/julia_gen/ -d -r -f
 
 cp rodinia/julia_cuda/ rodinia/julia_gen/ -d -r
 find rodinia/julia_gen/ -name "*.jl" -type f -delete
 
-julia ../src/execute.jl --input rodinia/julia_cuda --output rodinia/julia_gen --recursive --backend $backend
+julia -e 'using Juliana; main("--input rodinia/julia_cuda --output rodinia/julia_gen --recursive --backend='$backend'")'
 
 rm ExaTronKernels.jl/test/GEN.jl -f
-julia ../src/execute.jl --input ExaTronKernels.jl/test/CUDA.jl --output ExaTronKernels.jl/test/GEN.jl --backend $backend
+julia -e 'using Juliana; main("--input ExaTronKernels.jl/test/CUDA.jl --output ExaTronKernels.jl/test/GEN.jl --recursive --backend='$backend'")'
+
