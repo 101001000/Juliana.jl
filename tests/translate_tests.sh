481ab6f8bb2240d7b7827a122bc7bfb50918a18f
101001000 <kike.delacalle.montilla@gmail.com>
2024-10-22 16:04:53 +0200

    updated translate script

--- tests/translate_tests.sh
+++ tests/translate_tests.sh
@@ -1,12 +1,24 @@
-backend=CUDA
+julia -e "using Pkg; Pkg.develop(path=\"../\")"
 
-rm rodinia/julia_gen/ -d -r -f
+echo "Translating rodinia..."
 
-cp rodinia/julia_cuda/ rodinia/julia_gen/ -d -r
-find rodinia/julia_gen/ -name "*.jl" -type f -delete
+rm -rf rodinia-KA
+cp rodinia rodinia-KA -r
 
-julia -e 'using Juliana; main("--input rodinia/julia_cuda --output rodinia/julia_gen --recursive --backend='$backend'")'
+julia -e "using Juliana; translate_file(\"rodinia/julia_cuda/backprop/facetrain.jl\", \"rodinia-KA\")"
+julia -e "using Juliana; translate_file(\"rodinia/julia_cuda/bfs/bfs.jl\", \"rodinia-KA\")"
+julia -e "using Juliana; translate_file(\"rodinia/julia_cuda/hotspot/hotpot.jl\", \"rodinia-KA\")"
+julia -e "using Juliana; translate_file(\"rodinia/julia_cuda/leukocyte/leukocyte.jl\", \"rodinia-KA\")"
+julia -e "using Juliana; translate_file(\"rodinia/julia_cuda/lud/lud.jl\", \"rodinia-KA\")"
+julia -e "using Juliana; translate_file(\"rodinia/julia_cuda/nn/nn.jl\", \"rodinia-KA\")"
+julia -e "using Juliana; translate_file(\"rodinia/julia_cuda/nw/needle.jl\", \"rodinia-KA\")"
+julia -e "using Juliana; translate_file(\"rodinia/julia_cuda/particlefilter/particlefilter_double.jl\", \"rodinia-KA\")"
+julia -e "using Juliana; translate_file(\"rodinia/julia_cuda/pathfinder/pathfinder.jl\", \"rodinia-KA\")"
+julia -e "using Juliana; translate_file(\"rodinia/julia_cuda/streamcluster/streamcluster.jl\", \"rodinia-KA\")"
 
-rm ExaTronKernels.jl/test/GEN.jl -f
-julia -e 'using Juliana; main("--input ExaTronKernels.jl/test/CUDA.jl --output ExaTronKernels.jl/test/GEN.jl --recursive --backend='$backend'")'
 
+echo "Translating ExaTron..."
+
+rm -rf ExaTronKernels.jl-KA
+
+julia -e "using Juliana; translate_pkg(\"ExaTronKernels.jl\", \"ExaTronKernels.jl-KA\")"
